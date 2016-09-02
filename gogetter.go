bd9b4bc0a0288080103ff7efbc57f25ab5231708
Jasmit Tarang <J_Hack92@j-hack92.home>
2016-09-02 00:09:07 -0500

    initial commit

---
+++ gogetter.go
@@ -0,0 +1,26 @@
+package gogetter
+
+import (
+	"io/ioutil"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+//gorequest to open url and get the response and return it
+func GoTo(goURL string) string {
+	_, body, _ := gorequest.New().Get(goURL).End()
+	return body
+}
+
+// takes in the output filename
+func CreateDownload(filepath string) string {
+	return filepath
+}
+
+//writes the response from GoTO to a file that was declared in CreateDownload
+func SaveIt(bodyresponse string, download_out string) {
+	//convert to bytes or ioutil complains
+	byteit := []byte(bodyresponse)
+	// writes the output file with read/write permissions
+	ioutil.WriteFile(download_out, byteit, 0644)
+}
