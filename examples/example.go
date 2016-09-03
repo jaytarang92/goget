e1a3194f521e0b8e230d5ce9d33a889450419641
Jasmit Tarang <J_Hack92@Jennifers-MacBook-Pro.local>
2016-09-03 01:00:02 -0500

    md5 check is fully working will add more along the way

--- examples/example.go
+++ examples/example.go
@@ -6,11 +6,11 @@ import (
 	"github.com/jaytarang92/goget"
 )
 
-// gogetter -url=http://google.com -dst=/Users/J_Hack92/Desktop/index.html -md5sum=true
+// gogetter -url=http://google.com -dst=/Users/J_Hack92/Desktop/index.html -md5sum=1234567890123456789
 
 func main() {
 	// arguments accepted
-	md5Check := flag.Bool("md5sum", false, "Set to true if want to display md5")
+	md5Check := flag.String("md5sum", "", "If md5 is passed it will check it before saving file.")
 	url := flag.String("url", "", "url to file")
 	dest := flag.String("dst", "", "the output filename")
 	flag.Parse()
--- gogetter.go
+++ gogetter.go
@@ -4,23 +4,34 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/parnurzeal/gorequest"
 )
 
-//gorequest to open url and get the response and return it
+//GoTo gorequest to open url and get the response and return it
 func GoTo(goURL string) string {
 	_, body, _ := gorequest.New().Get(goURL).End()
 	return body
 }
 
-//writes the response from GoTO to a file (download_out)
-func SaveIt(bodyresponse string, download_out string, checksum bool) {
+//SaveIt writes the response from GoTo to a file (download_out)
+func SaveIt(bodyresponse string, downloadOut string, checksum string) {
 	//convert to bytes or ioutil complains
 	byteit := []byte(bodyresponse)
-	if checksum {
-		fmt.Printf("\nmd5:\t%x\n\n", md5.Sum(byteit))
+	if len(checksum) != 0 {
+		//assign to hash to output as string
+		hash := md5.Sum(byteit)
+		strhash := fmt.Sprintf("%x", hash)
+		//compare md5
+		if checksum == strhash {
+			fmt.Printf("Checksums Match! Saving to %s\n", downloadOut)
+		} else {
+			fmt.Printf("\tChecksums don't match exiting!\n")
+			fmt.Printf("\tPassed:\t%s\n\tActual:\t%x\n", checksum, hash)
+			os.Exit(1)
+		}
 	}
 	// writes the output file with read/write permissions
-	ioutil.WriteFile(download_out, byteit, 0644)
+	ioutil.WriteFile(downloadOut, byteit, 0644)
 }
